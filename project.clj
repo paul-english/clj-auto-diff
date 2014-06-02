2687c2dbb01fe53ea87da9717af88d9283baf282
Paul English <paul@onfrst.com>
2014-06-01 21:53:28 -0600

    Set metadatain project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-auto-diff "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-auto-diff "0.1.0"
+  :description "Automatic differentiation library"
+  :url "http://github.com/log0ymxm/clj-auto-diff"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
